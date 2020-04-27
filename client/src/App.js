4b7d4e7a9ca31440244f2192d4fbb8fa170ad91e
Mahdieh019 <mahsanasr2016@gmail.com>
2020-04-27 11:00:03 +0200

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Fragment } from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+
 import './App.css';
 
-class App extends Component {
-  render() {
+const App=()=> {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <div className='container'>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/about' component={About}/>
+                    </Switch>
+                </div>
+            </Fragment> 
+       </Router>
+    )
 }
 
 export default App;
