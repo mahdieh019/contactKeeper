27caa61c6075347ad4648ce3edc26b463d01a711
Mahdieh019 <mahsanasr2016@gmail.com>
2020-06-03 14:49:40 +0200

    last update

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -28,7 +28,7 @@ const ContactForm = () => {
     });
     const {name,email,phone,type}=contact;
 
-    const onChange= e =>setContact({...contact,[e.target.name]:e.target.value })
+    const onChange= e =>setContact({...contact, [e.target.name]: e.target.value })
 
     const onSubmit= e =>{
         e.preventDefault();
